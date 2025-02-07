a341eedccfb1513f0a0a85e704342bd39113906d
Şey XR <seytantuyux@gmail.com>
2025-02-07 07:58:17 -0600

    page config

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,37 +1,35 @@
 /// <reference types="cypress" />
 
-describe('My First Test', () => {
-  it('First test code', () => {
-
-    cy.visit('http://localhost:4200/')
-    cy.contains('Forms').click()
-    cy.contains('Form Layouts').click()
+describe("My First Test", () => {
+  it("First test code", () => {
+    cy.visit("http://localhost:4200/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
     //by Tag Name
-    cy.get('input')
+    cy.get("input");
 
     //by  ID
-    cy.get('#inputEmail')
+    cy.get("#inputEmail");
 
     //by Class value
-    cy.get('.input-full-width')
+    cy.get(".input-full-width");
 
     //by Attribute name
-    cy.get('[fullwidth]')
+    cy.get("[fullwidth]");
 
     //by Attribute name and value
-    cy.get('[placeholder="Email"]')
+    cy.get('[placeholder="Email"]');
 
     // by entire Calass value
-    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
 
     // by two attributes
-    cy.get('[placeholder="Email"][fullwidth]')
+    cy.get('[placeholder="Email"][fullwidth]');
 
     //by tag, attribute ID and Class
-    cy.get('input[placeholder="Email"].input-full-width')
+    cy.get('input[placeholder="Email"].input-full-width');
 
     // by cypress test ID
-    cy.get('[data-cy="imputEmail1"]')
-
-  })
-})
+    cy.get('[data-cy="imputEmail1"]');
+  });
+});
